70359038064c3ea31bb841718fef2e122c7515b2
aaronamm <theeranitp@gmail.com>
2019-07-08 13:00:07 +0700

    Connect database with new created Windows Authentication login

---
+++ create-web-login-user-assign-role.sql
@@ -0,0 +1,14 @@
+﻿USE MASTER
+GO
+
+CREATE LOGIN [aaron-mac-pro\web_user] FROM WINDOWS WITH DEFAULT_DATABASE = CodesanookMeetup
+GO
+
+USE CodesanookMeetup
+GO
+
+CREATE USER web_user FOR LOGIN [aaron-mac-pro\web_user] WITH DEFAULT_SCHEMA = dbo
+GO
+
+EXEC sp_addrolemember @rolename = 'DB_DataReader', @membername = 'web_user'
+GO
---
+++ drop-web-login-user-remove-role.sql
@@ -0,0 +1,14 @@
+﻿USE CodesanookMeetup
+GO
+
+EXEC sp_droprolemember @rolename = 'DB_DataReader', @membername ='web_user'
+GO
+
+DROP USER web_user 
+GO
+
+USE MASTER
+GO
+
+DROP LOGIN [aaron-mac-pro\web_user];  
+GO
